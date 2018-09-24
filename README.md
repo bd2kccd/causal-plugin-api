215e7867771692408fcd649bd7bb4e4ae25963e3
chirayukong <chirayu.kong@gmail.com>
2018-09-24 11:31:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# causal-plugin-api
---
+++ src/main/java/edu/pitt/dbmi/tetrad/plugin/api/PluginScoreWrapper.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.pitt.dbmi.tetrad.plugin.api;
+
+import org.pf4j.ExtensionPoint;
+
+import edu.cmu.tetrad.algcomparison.score.ScoreWrapper;
+
+/**
+ * Sep 21, 2018 6:46:20 PM
+ *
+ * @author Chirayu Wongchokprasitti, PhD (chw20@pitt.edu)
+ *
+ */
+public interface PluginScoreWrapper extends ExtensionPoint, ScoreWrapper {
+
+}
