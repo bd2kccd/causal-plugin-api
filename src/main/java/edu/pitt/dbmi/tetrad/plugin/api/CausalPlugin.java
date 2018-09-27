a50618fbc03f366d7eb2dd3dc306beee4998d7c7
chirayukong <chirayu.kong@gmail.com>
2018-09-27 18:22:53 -0400

    Removed tetrad-lib and simplified extensionpoint to be only one place.

--- src/main/java/edu/pitt/dbmi/tetrad/plugin/api/CausalPlugin.java
+++ src/main/java/edu/pitt/dbmi/tetrad/plugin/api/CausalPlugin.java
@@ -2,14 +2,12 @@ package edu.pitt.dbmi.tetrad.plugin.api;
 
 import org.pf4j.ExtensionPoint;
 
-import edu.cmu.tetrad.algcomparison.algorithm.Algorithm;
-
 /**
  * Sep 20, 2018 5:06:57 PM
  *
  * @author Chirayu Kong Wongchokprasitti, PhD (chw20@pitt.edu)
  *
  */
-public interface PluginAlgorithm extends ExtensionPoint, Algorithm {
-
+public interface CausalPlugin extends ExtensionPoint {
+	
 }
